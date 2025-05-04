f0b28f3b8cf34c2c5ee0ab4d4f15c5d552563ae7
Matthew Marriott <72930889+matthewmarriott@users.noreply.github.com>
2025-05-04 11:14:19 +0100

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: 'export',
-  images: {
-    unoptimized: true, // Required for static export if you're using next/image
-  }
-};
-
-module.exports = nextConfig;
